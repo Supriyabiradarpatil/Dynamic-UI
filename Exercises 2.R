8e6f7d90f07f5cb9bd16c795ae69f85512ef64c5
Supriyabiradarpatil <supriybiradarpatil@gmail.com>
2024-11-11 16:53:45 +0530

    Add files via upload

---
+++ Exercises 2.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(dplyr, warn.conflicts = FALSE)
+
+
+
+ui <- fluidPage(
+  actionButton("go", "Enter password"),
+  textOutput("text")
+)
+server <- function(input, output, session) {
+  observeEvent(input$go, {
+    showModal(modalDialog(
+      passwordInput("password", NULL),
+      title = "Please enter your password"
+    ))
+  })
+  
+  output$text <- renderText({
+    if (!isTruthy(input$password)) {
+      "No password"
+    } else {
+      "Password entered"
+    }
+  })
+}
+
+shinyApp(ui, server)
---
+++ Freezing reactive inputs.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(dplyr, warn.conflicts = FALSE)
+
+ui <- fluidPage(
+  selectInput("dataset", "Choose a dataset", c("pressure", "cars")),
+  selectInput("column", "Choose column", character(0)),
+  verbatimTextOutput("summary")
+)
+
+server <- function(input, output, session) {
+  dataset <- reactive(get(input$dataset, "package:datasets"))
+  
+  observeEvent(input$dataset, {
+    updateSelectInput(inputId = "column", choices = names(dataset()))
+  })
+  
+  output$summary <- renderPrint({
+    summary(dataset()[[input$column]])
+  })
+}
+shinyApp(ui, server)
